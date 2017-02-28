a97804aaf6d674d27c165fe8a2d40c42f67a7b31
Lia <lchau033@uottawa.ca>
2017-02-28 13:37:42 -0500

    initial commit

---
+++ sources/org/cloudbus/cloudsim/power/PowerVmAllocationPolicyMigrationPowerUsageEfficiency.java
@@ -0,0 +1,21 @@
+package org.cloudbus.cloudsim.power;
+
+import java.util.List;
+
+import org.cloudbus.cloudsim.Host;
+
+public class PowerVmAllocationPolicyMigrationPowerUsageEfficiency extends PowerVmAllocationPolicyMigrationAbstract {
+
+	public PowerVmAllocationPolicyMigrationPowerUsageEfficiency(List<? extends Host> hostList,
+			PowerVmSelectionPolicy vmSelectionPolicy) {
+		super(hostList, vmSelectionPolicy);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean isHostOverUtilized(PowerHost host) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
